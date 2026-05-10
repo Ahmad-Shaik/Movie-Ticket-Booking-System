36c193954819a89cc34cd5557d78abc24065aab0
Ahmad-Shaik <136552189+Ahmad-Shaik@users.noreply.github.com>
2026-05-10 07:00:51 +0000

    3

--- js/firebase.js
+++ js/firebase.js
@@ -1,22 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyC5OfvWEDQX26FQtFNN-363ci1r6wEcIlA",
   authDomain: "dolly-movies.firebaseapp.com",
-  databaseURL: "https://dolly-movies-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "dolly-movies",
   storageBucket: "dolly-movies.firebasestorage.app",
   messagingSenderId: "252494791922",
-  appId: "1:252494791922:web:03ef45b35636c397ddfc6b",
-  measurementId: "G-PXQBTP0MV7"
+  appId: "1:252494791922:web:03ef45b35636c397ddfc6b"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
