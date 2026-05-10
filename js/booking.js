e663e43abce36a3f98d3ddb24cefe53afcfadaa2
Ahmad-Shaik <136552189+Ahmad-Shaik@users.noreply.github.com>
2026-05-10 08:51:23 +0000

    2

---
+++ js/booking.js
@@ -0,0 +1,366 @@
+import {
+  auth,
+  db
+} from './firebase.js';
+
+import {
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+  collection,
+  getDocs,
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+// ======================
+// ELEMENTS
+// ======================
+
+const moviesContainer =
+document.getElementById('moviesContainer');
+
+const logoutBtn =
+document.getElementById('logoutBtn');
+
+const searchInput =
+document.getElementById('searchInput');
+
+const placeFilter =
+document.getElementById('placeFilter');
+
+const theaterFilter =
+document.getElementById('theaterFilter');
+
+const userName =
+document.getElementById('userName');
+
+// ======================
+// DATA
+// ======================
+
+let allMovies = [];
+
+// ======================
+// AUTH CHECK
+// ======================
+
+onAuthStateChanged(auth, async (user) => {
+
+  if(!user){
+
+    window.location.href =
+    './index.html';
+
+    return;
+  }
+
+  loadUserData(user.uid);
+
+  loadMovies();
+
+});
+
+// ======================
+// LOAD USER
+// ======================
+
+async function loadUserData(uid){
+
+  try {
+
+    const userRef =
+    doc(db, 'users', uid);
+
+    const userSnap =
+    await getDoc(userRef);
+
+    if(userSnap.exists()){
+
+      const user =
+      userSnap.data();
+
+      if(user.role === 'admin'){
+
+        userName.innerHTML =
+        '👑 ADMIN';
+
+      } else {
+
+        userName.innerHTML =
+        `👤 ${user.name}`;
+
+      }
+
+    }
+
+  } catch(error){
+
+    console.log(error);
+
+  }
+
+}
+
+// ======================
+// LOGOUT
+// ======================
+
+logoutBtn.addEventListener('click', async () => {
+
+  await signOut(auth);
+
+  window.location.href =
+  './index.html';
+
+});
+
+// ======================
+// LOAD MOVIES
+// ======================
+
+async function loadMovies(){
+
+  try {
+
+    moviesContainer.innerHTML = `
+      <h3 class="text-center text-light">
+        Loading Movies...
+      </h3>
+    `;
+
+    const querySnapshot =
+    await getDocs(collection(db, 'movies'));
+
+    allMovies = [];
+
+    let places = [];
+
+    let theaters = [];
+
+    querySnapshot.forEach((docSnap) => {
+
+      const movie = {
+        id: docSnap.id,
+        ...docSnap.data()
+      };
+
+      allMovies.push(movie);
+
+      // UNIQUE PLACE
+
+      if(!places.includes(movie.place)){
+
+        places.push(movie.place);
+
+      }
+
+      // UNIQUE THEATER
+
+      if(!theaters.includes(movie.theater)){
+
+        theaters.push(movie.theater);
+
+      }
+
+    });
+
+    // PLACE FILTER
+
+    placeFilter.innerHTML = `
+      <option value="">
+        Select Place
+      </option>
+    `;
+
+    places.forEach(place => {
+
+      placeFilter.innerHTML += `
+
+        <option value="${place}">
+          ${place}
+        </option>
+
+      `;
+
+    });
+
+    // THEATER FILTER
+
+    theaterFilter.innerHTML = `
+      <option value="">
+        Select Theater
+      </option>
+    `;
+
+    theaters.forEach(theater => {
+
+      theaterFilter.innerHTML += `
+
+        <option value="${theater}">
+          ${theater}
+        </option>
+
+      `;
+
+    });
+
+    renderMovies(allMovies);
+
+  } catch(error){
+
+    console.log(error);
+
+    moviesContainer.innerHTML = `
+      <h3 class="text-danger text-center">
+        Failed To Load Movies
+      </h3>
+    `;
+
+  }
+
+}
+
+// ======================
+// RENDER MOVIES
+// ======================
+
+function renderMovies(movies){
+
+  moviesContainer.innerHTML = '';
+
+  // EMPTY
+
+  if(movies.length === 0){
+
+    moviesContainer.innerHTML = `
+      <h3 class="text-center text-light">
+        No Movies Available
+      </h3>
+    `;
+
+    return;
+  }
+
+  movies.forEach(movie => {
+
+    moviesContainer.innerHTML += `
+
+      <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+
+        <div class="neon-card movie-card h-100 p-3">
+
+          <img
+            src="${movie.posterUrl}"
+            class="img-fluid rounded mb-3"
+            style="
+              height:350px;
+              object-fit:cover;
+            "
+          >
+
+          <h4 class="neon-heading mb-3">
+            ${movie.movieName}
+          </h4>
+
+          <p class="text-light">
+            📍 ${movie.place}
+          </p>
+
+          <p class="text-light">
+            🎬 ${movie.theater}
+          </p>
+
+          <p class="text-light">
+            🕒 ${movie.showTime}
+          </p>
+
+          <p class="text-light">
+            💰 ₹${movie.ticketPrice}
+          </p>
+
+          <a
+            href="./booking.html?id=${movie.id}"
+            class="btn neon-btn mt-auto"
+          >
+            Book Now
+          </a>
+
+        </div>
+
+      </div>
+
+    `;
+
+  });
+
+}
+
+// ======================
+// FILTER MOVIES
+// ======================
+
+function filterMovies(){
+
+  const search =
+  searchInput.value.toLowerCase();
+
+  const place =
+  placeFilter.value;
+
+  const theater =
+  theaterFilter.value;
+
+  const filteredMovies =
+  allMovies.filter(movie => {
+
+    return (
+
+      movie.movieName
+      .toLowerCase()
+      .includes(search)
+
+      &&
+
+      (
+        place === ''
+        ||
+        movie.place === place
+      )
+
+      &&
+
+      (
+        theater === ''
+        ||
+        movie.theater === theater
+      )
+
+    );
+
+  });
+
+  renderMovies(filteredMovies);
+
+}
+
+// ======================
+// EVENTS
+// ======================
+
+searchInput.addEventListener(
+  'input',
+  filterMovies
+);
+
+placeFilter.addEventListener(
+  'change',
+  filterMovies
+);
+
+theaterFilter.addEventListener(
+  'change',
+  filterMovies
+);
